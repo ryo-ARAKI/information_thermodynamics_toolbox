fe5e0de2594acafe28038e36b547b7df6e4e0aab
ryo-ARAKI <r_araki@fm.me.es.osaka-u.ac.jp>
2021-10-06 18:49:36 +0200

    Compute Shannon entropy & KL divergence

--- information_thermodynamics.py
+++ information_thermodynamics.py
@@ -13,3 +13,26 @@
 #   - ***Note: This article employs np.log2 instead of np.log***
 #
 ########################################################################
+
+import numpy as np
+from scipy import stats
+
+
+# main
+if __name__ == '__main__':
+
+    # Entropy
+    x = [0.95, 0.05]
+    print("Entropy of x=", x)
+    print("Scipy:", '{:6.4f}'.format(
+        stats.entropy(x, base=np.e)
+    ))
+    print("")
+
+    # Relative entropy (Kullback-Leibler divergence)
+    y = [0.2, 0.8]
+    print("Relative entropy of x=", x, "and y=", y)
+    print("Scipy:", '{:6.4f}'.format(
+        stats.entropy(pk=x, qk=y, base=np.e)
+    ))
+    print("")

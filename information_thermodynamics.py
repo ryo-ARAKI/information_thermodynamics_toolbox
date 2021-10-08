Fix computation of py in conditional_probability

information_thermodynamics.py
@@ -72,7 +72,7 @@ class InformationThermodynamics():
         if p.ndim != 2:
             raise ValueError('Input array p must be 2D ndarray')
 
-        py = self.marginal_probability(p)
+        py = self.marginal_probability(p.transpose()).transpose()
         px_1 = np.ones_like(py)
 
         return p / np.tensordot(px_1, py, axes=0)
8209eef62ebce7cc42fda49fd32f19ca633e9f02
ryo-ARAKI <r_araki@fm.me.es.osaka-u.ac.jp>
2021-12-09 15:22:18 +0100

    Add `information_thermodynamics.jl`

---
+++ information_thermodynamics.jl
@@ -0,0 +1,13 @@
+#=
+Implement quantities defined in information thermodynamics theory.
+09/12/2021: first commit
+
+References:
+    - 非平衡統計力学の基礎理論
+        - http:/http://sosuke110.com/NoteBenkyokai.pdf
+    - For implementation.
+        - Calculating Mutual Information in Python
+    - https://www.roelpeters.be/calculating-mutual-information-in-python/
+    - For test of implemented functions and libraries.
+        - ***Note: This article employs np.log2 instead of np.log***
+=#

2f6dd9b1fb84616d9b69bca3f5a7f81c83679ac1
ryo-ARAKI <r_araki@fm.me.es.osaka-u.ac.jp>
2021-12-09 17:09:15 +0100

    Small fix of header comment & black line

--- information_thermodynamics.jl
+++ information_thermodynamics.jl
@@ -3,13 +3,13 @@ Implement quantities defined in information thermodynamics theory.
 09/12/2021: first commit
 
 References:
-    - 非平衡統計力学の基礎理論
-        - http:/http://sosuke110.com/NoteBenkyokai.pdf
+- 非平衡統計力学の基礎理論
+    - http:/http://sosuke110.com/NoteBenkyokai.pdf
     - For implementation.
-        - Calculating Mutual Information in Python
+- Calculating Mutual Information in Python
     - https://www.roelpeters.be/calculating-mutual-information-in-python/
     - For test of implemented functions and libraries.
-        - ***Note: This article employs np.log2 instead of np.log***
+    - ***Note: This article employs np.log2 instead of np.log***
 =#
 
 module InformationThermodynamics
@@ -91,7 +91,6 @@ function conditional_entropy(p)
     px_cond_y = conditional_probability(p)
 
     return sum(-p .* log.(px_cond_y))
-
 end
 
 """
